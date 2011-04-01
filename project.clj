c9d84f9d2764e85d1b36fbd399e38e09f09de2cf
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-04-01 09:19:31 +0200

    compojure version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject status-codes "0.2-SNAPSHOT"
   :description "Easy status codes for compojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [compojure "0.5.3"]
+                 [compojure "0.6.2"]
                  [ring-mock "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
