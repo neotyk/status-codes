355eb4c6cfd0b92c8e1014594651b6e5a531d73c
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-04-01 14:08:15 +0200

    0.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject status-codes "0.2-SNAPSHOT"
+(defproject status-codes "0.2"
   :description "Easy status codes for compojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [compojure "0.6.2"]
