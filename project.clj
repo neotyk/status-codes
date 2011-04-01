5550943971b7c78dceeb70e91b30bc1d1865e983
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-04-01 09:18:59 +0200

    next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject status-codes "0.1"
+(defproject status-codes "0.2-SNAPSHOT"
   :description "Easy status codes for compojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [compojure "0.5.3"]
