f9c592020e2523040ae8b282f3d1aaa22c9d2a59
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-04-07 01:41:43 +0200

    Next release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject status-codes "0.3-SNAPSHOT"
+(defproject status-codes "0.3"
   :description "Easy status codes for ring and compojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [compojure "0.6.2"]
