8ce3fcbe313d5d1f4ce0feb7bcf335a942243095
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-09-21 14:38:19 +0200

    New version with patches by https://github.com/joodie
    Also Clojure updated to 1.2.1.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject status-codes "0.3"
+(defproject status-codes "0.4"
   :description "Easy status codes for ring and compojure."
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [compojure "0.6.2"]
                  [ring-mock "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
