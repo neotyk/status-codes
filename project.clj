4b3ec521bccd7c06a5cc1b859b6856372a610a1d
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-10-11 15:13:31 +0200

    Release 0.5 with fall through fix.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject status-codes "0.4"
+(defproject status-codes "0.5"
   :description "Easy status codes for ring and compojure."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [compojure "0.6.2"]
